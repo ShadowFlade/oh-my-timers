a31121d095509ba660399f4efdd84aa4837ff4eb
Vladimir Drugov <wolffdrugov@yandex.ru>
2025-10-01 20:06:38 +0300

    master:test deploy

--- pkg/handlers/timers.go
+++ pkg/handlers/timers.go
@@ -126,6 +126,7 @@ func (this *TimerHandler) PauseTimer(w http.ResponseWriter, r *http.Request) {
 
 		return
 	}
+	fmt.Println("TEST4")
 	fmt.Println("TEST3")
 	fmt.Println("TEST2")
 	fmt.Println("TEST")
